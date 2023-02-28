5eefcea5ef4453040c404547cef0f3a09421ccc1
aleksey <semykov@mail.ru>
2023-02-28 22:43:46 +0700

    add lazy/Suspense

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import './App.css';
-import Music from "./components/Music/Music";
-import News from "./components/News/News";
-import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileConteiner";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import LoginContainer from "./components/Login/LoginContainer";
-import React, {useEffect} from "react";
+import React, {useEffect, Suspense} from "react";
 import {connect, Provider} from "react-redux";
 import {initializedApp} from "./redux/app-reducer";
 import Preloader from "./common/Preloader/Preloader";
 import store from "./redux/redux-store";
 
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileConteiner"));
+const UsersContainer = React.lazy(() => import("./components/Users/UsersContainer"));
+const LoginContainer = React.lazy(() => import("./components/Login/LoginContainer"));
+const Music = React.lazy(() => import("./components/Music/Music"));
+const News = React.lazy(() => import("./components/News/News"));
+const Settings = React.lazy(() => import("./components/Settings/Settings"));
 
 const App = (props) => {
     useEffect(() => {
         props.initializedApp()
     })
     if (!props.initialized) {
-        return <Preloader />
+        return <Preloader/>
     }
     return (
         <Provider store={store}>
@@ -29,15 +29,19 @@ const App = (props) => {
                 <HeaderContainer/>
                 <NavbarContainer/>
                 <div className="app-wrapper-content">
-                    <Routes>
-                        <Route path='/profile/:userId' element={<ProfileContainer/>}/>
-                        <Route path='/dialogs/*' element={<DialogsContainer/>}/>
-                        <Route path='/music/*' element={<Music/>}/>
-                        <Route path='/news/*' element={<News/>}/>
-                        <Route path='/users/*' element={<UsersContainer/>}/>
-                        <Route path='/settings/*' element={<Settings/>}/>
-                        <Route path='/login/*' element={<LoginContainer/>}/>
-                    </Routes>
+                    <Suspense fallback={<Preloader/>}>
+                        <section>
+                            <Routes>
+                                <Route path='/login/*' element={<LoginContainer/>}/>
+                                <Route path='/profile/:userId' element={<ProfileContainer/>}/>
+                                <Route path='/dialogs/*' element={<DialogsContainer/>}/>
+                                <Route path='/music/*' element={<Music/>}/>
+                                <Route path='/news/*' element={<News/>}/>
+                                <Route path='/users/*' element={<UsersContainer/>}/>
+                                <Route path='/settings/*' element={<Settings/>}/>
+                            </Routes>
+                        </section>
+                    </Suspense>
                 </div>
             </div>
         </Provider>
