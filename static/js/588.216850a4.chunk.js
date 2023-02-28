cd41fa1e6d92995f5e1a4f3f74e115b945081d13
AlekseySemukov <ches488@gmail.com>
2023-03-01 03:03:27 +0700

    Updates

---
+++ static/js/588.216850a4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfirstapp=self.webpackChunkfirstapp||[]).push([[588],{1588:function(n,t,e){e.r(t),e.d(t,{default:function(){return u}});var r=e(184),u=function(n){return(0,r.jsx)("div",{children:"Settings"})}}}]);
+//# sourceMappingURL=588.216850a4.chunk.js.map
---
+++ static/js/876.85567879.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfirstapp=self.webpackChunkfirstapp||[]).push([[876],{9876:function(n,u,r){r.r(u),r.d(u,{default:function(){return e}});var t=r(184),e=function(n){return(0,t.jsx)("div",{children:"Music"})}}}]);
+//# sourceMappingURL=876.85567879.chunk.js.map
